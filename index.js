7a506a9335897348adc32206f409721b35b03124
lanarokip <lankipkoecharo@gmail.com>
2021-07-17 11:35:12 +0300

    added the section where it determins the Akan name by taking the day as the input

---
+++ index.js
@@ -0,0 +1,58 @@
+var gender = prompt("input gender")
+if (gender == 'male' ){
+        var A = prompt("enter date");
+        switch (A)
+        {
+           case 'sunday' : document.write("your AKAN name is KWASI<br/>");
+           break;
+           
+           case 'monday': document.write("your AKAN name is KWADWO<br />");
+           break;
+        
+           case 'tuesday': document.write("your AKAN name is KWABENA<br />");
+           break;
+        
+           case 'wednesday': document.write("your AKAN name is KWAKU<br />");
+           break;
+        
+           case 'thursday': document.write("your AKAN name is Yaw<br />");
+           break;
+        
+           case 'friday': document.write("your AKAN name is KOFI<br />");
+           break;
+           
+           case 'saturday': document.write("your Akan name is {Kwame}");
+           break;
+        
+           default:  document.write("Unknown grade<br />")
+        }
+        document.write("Exiting switch block");
+        }
+        else
+        var A = prompt("enter date");
+        switch (A)
+        {
+           case 'sunday' : document.write("your AKAN name is AKOSUA<br/>");
+           break;
+           
+           case 'monday': document.write("your AKAN name is ADWOA<br />");
+           break;
+        
+           case 'tuesday': document.write("your AKAN name is ABENAA<br />");
+           break;
+        
+           case 'wednesday': document.write("your AKAN name is AKUA<br />");
+           break;
+        
+           case 'thursday': document.write("your AKAN name is YAA<br />");
+           break;
+        
+           case 'friday': document.write("your AKAN name is AFUA<br />");
+           break;
+           
+           case 'saturday': document.write("your Akan name is {Ama}");
+           break;
+        
+           default:  document.write("Unknown grade<br />")
+        }
+        
